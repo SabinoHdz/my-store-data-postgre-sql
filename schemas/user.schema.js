f49bdd89fba8d6796322ad2f77485c3f9220522b
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-04-22 16:40:00 -0600

    realizar after table a user con migraciones y agregar campo role

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -5,18 +5,21 @@ const name = Joi.string().min(10).max(50);
 const user=Joi.string().min(5).max(20);
 const email=Joi.string().email();
 const password=Joi.string().min(8).max(20);
+const role=Joi.string().min(5);
 const createUserSchema = Joi.object({
   name: name.required(),
   user: user.required(),
   email:email.required(),
-  password:password.required()
+  password:password.required(),
+  role:role.required()
 });
 
 const updateUserSchema = Joi.object({
   name: name,
   user: user,
   email:email,
-  password:password
+  password:password,
+  role:role
 });
 
 const getUserSchema = Joi.object({
